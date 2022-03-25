d8ef61084c71340484ecdaa0566967bef6736be7
Sujith Simon <sujith.simon888@gmail.com>
2022-03-25 15:33:28 -0400

    added company and companyreview models

---
+++ Skilled Force VS 22/Models/DB/Company.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Skilled_Force_VS_22.Models.DB
+{
+    public class Company
+    {
+
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string CompanyId { get; set; }
+
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public virtual User User { get; set; }
+
+    }
+}
---
+++ Skilled Force VS 22/Models/DB/CompanyReview.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Skilled_Force_VS_22.Models.DB
+{
+    public class CompanyReview
+    {
+
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string CompanyRatingId { get; set; }
+
+
+        [Required]
+        public string CompanyId { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+        [Required]
+        public string comment { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public virtual Company Company { get; set; }
+
+
+    }
+}
